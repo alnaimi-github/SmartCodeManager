79fbeeeecd6a1952d4c2c66b581a86189efdd3dd
alnaimi-github <alnymybdalwas1@gmail.com>
2024-11-26 19:46:58 +0300

    Add project files.

---
+++ SmartCodeManager/Controllers/WebhookController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartCodeManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        [HttpPost("git-events")]
+        public IActionResult ReceiveGitEvent([FromBody] dynamic payload)
+        {
+            Console.WriteLine($"Received Webhook: {payload}");
+
+            return Ok("Webhook received successfully!");
+        }
+    }
+}
